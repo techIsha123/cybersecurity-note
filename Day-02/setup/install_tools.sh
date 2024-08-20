f8248e2c8120980a12c603b6ca3bef47a4d04926
techIsha123 <ishajangra2005@gmail.com>
2024-08-20 13:01:26 +0530

    Create install_tools.sh

---
+++ Day-02/setup/install_tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install necessary tools
+echo "Installing cybersecurity tools..."
+
+# Example: Install Wireshark
+sudo apt-get update
+sudo apt-get install -y wireshark
+
+echo "Tool installation complete."
